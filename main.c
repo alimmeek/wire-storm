5ec4ba4b9dcd586cfd514de44d4c63b49485f20a
Alia Meek <alimmeek12@gmail.com>
2025-06-27 08:44:00 +0100

    Added connection stuff (doesn't compile)

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #define SOURCE_PORT 33333
 #define DESTINATION_PORT 44444
 
+
 void make_socket(struct sockaddr_in *server_ptr, int *server_sock_ptr, unsigned short port_no) {
     struct sockaddr_in server = *server_ptr;
     int server_sock = *server_sock_ptr;
@@ -32,6 +33,136 @@ void make_socket(struct sockaddr_in *server_ptr, int *server_sock_ptr, unsigned 
 
     *server_sock_ptr = server_sock;
     *server_ptr = server;
+}
+
+client_t *add_client(server_t *s, int fd) {
+    client_t *cli = (client_t *) malloc(sizeof(client_t));
+
+    if (!cli) {
+        printf("Error: cannot allocate memory for client\n");
+        exit(1);
+    }
+
+    memset(cli, 0, sizeof(client_t));
+    cli->fd = fd;
+    cli->id = s->counter++;
+    cli->msg = NULL;
+    cli->next = s->head;
+    s->head = cli;
+    
+    return cli;
+}
+
+client_t *findClient(server_t *s, int fd) {
+    client_t *tmp = s->head;
+
+    while (tmp && tmp->fd != fd)
+        tmp = tmp->next;
+    return tmp;
+}
+
+void remove_client(server_t *s, int fd) {
+    client_t *tmp = s->head;
+    client_t *prev = NULL;
+
+    while (tmp && tmp->fd != fd) {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    if (tmp) {
+        if (prev)
+            prev->next = tmp->next;
+        else
+            s->head = tmp->next;
+        freeClient(tmp);
+    }
+}
+
+void send_notification(server_t *s, int fd, char *msg) {
+    client_t *cli = s->head;
+    while (cli) {
+        if (FD_ISSET(cli->fd, &s->writefds) && cli->fd != fd)
+            if (send(cli->fd, msg, strlen(msg), 0) < 0) {
+                printf("Error: failed to send message to client %d\n", cli->id);
+                close(cli->fd);
+                FD_CLR(cli->fd, &s->active_fds);
+            }
+        cli = cli->next;
+    }
+}
+
+void sendMessage(server_t *s, client_t *cli) {
+    char buf[127];
+    char *msg;
+    while (extract_message(&cli->msg, &msg))
+    {
+        if (FD_ISSET(cli->fd, &s->writefds)) {
+            sprintf(buf, "client %d: ", cli->id);
+            send_notification(s, cli->fd, buf);
+            send_notification(s, cli->fd, msg);
+            free(msg);
+        }
+    }
+}
+
+void deregisterClient(server_t *s, int fd, int cli_id) {
+    char buf[127];
+    sprintf(buf, "server: client %d just left\n", cli_id);
+    send_notification(s, fd, buf);
+    FD_CLR(fd, &s->active_fds);
+    remove_client(s, fd);
+}
+
+void processMessage(server_t *s, int fd) {
+    char buf[4096];
+    client_t *cli = findClient(s, fd);
+    if (!cli) return;
+    int read_bytes = recv(fd, buf, sizeof(buf) - 1, 0);
+    if (read_bytes <= 0) {
+        deregisterClient(s, fd, cli->id);
+    } else {
+        buf[read_bytes] = '\0';
+        cli->msg = str_join(cli->msg, buf);
+        sendMessage(s, cli);
+    }
+}
+
+void registerClient(server_t *s, int fd) {
+    client_t *cli = add_client(s, fd);
+    char buf[127];
+    if (!cli) fatalError(s);
+    FD_SET(cli->fd, &s->active_fds);
+    if (cli->fd > s->max_fd)
+        s->max_fd = cli->fd;
+    sprintf(buf, "server: client %d just arrived\n", cli->id);
+    send_notification(s, fd, buf);
+}
+
+void acceptRegistration(server_t *s) {
+    struct sockaddr_in cli; 
+
+    socklen_t len = sizeof(cli);
+    int fd = accept(s->sockfd, (struct sockaddr *)&cli, &len);
+    if (fd < 0) fatalError(s);
+    registerClient(s, fd);
+}
+
+void monitor_fds(server_t *s) {
+    if (select(s->max_fd + 1, &s->readfds, &s->writefds, NULL, NULL) < 0) fatalError(s);
+    int fd = 0;
+    while (fd <= s->max_fd) {
+        if (FD_ISSET(fd, &s->readfds))
+            (fd == s->sockfd) ? acceptRegistration(s) : processMessage(s, fd);
+        fd++;
+    }
+}
+
+void handle_connection(server_t *s) {
+    while (1) {
+        s->readfds = s->active_fds;
+        s->writefds = s->active_fds;
+        monitor_fds(s);
+    }
 }
 
 int main() {
--- types.h
+++ types.h
@@ -9,4 +9,22 @@ typedef struct ctmp {
     char* data;
 } ctmp_t;
 
+
+typedef struct client {
+    int fd;
+    int id;
+    char *msg;
+    struct client *next;
+} client_t;
+
+typedef struct server {
+    int sockfd;
+    int max_fd;
+    int port;
+    int counter;
+    fd_set readfds, writefds, active_fds;
+    struct sockaddr_in addr;
+    client_t *head;
+} server_t;
+
 #endif
