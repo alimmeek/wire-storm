50bea44ff7687f7289ac295c950ff5b100a75b2b
Alia Meek <u2106850@warwick.ac.uk>
2025-06-27 09:39:12 +0100

    TCP server now waits on both ports

--- main.c
+++ main.c
@@ -13,26 +13,61 @@
 #define DESTINATION_PORT 44444
 
 
-void make_socket(struct sockaddr_in *server_ptr, int *server_sock_ptr, unsigned short port_no) {
-    struct sockaddr_in server = *server_ptr;
-    int server_sock = *server_sock_ptr;
+void make_socket(struct sockaddr_in *server, int *server_sock_ptr, unsigned short port_no) {
+    int server_sock;
 
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("Error: cannot create socket\n");
         exit(1);
     }
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_port = htons(port_no);
+    server->sin_family = AF_INET;
+    server->sin_addr.s_addr = inet_addr("127.0.0.1");
+    server->sin_port = htons(port_no);
 
-    if (bind(server_sock, (struct sockaddr *) server_ptr, sizeof(server)) < 0) {
+    if (bind(server_sock, (struct sockaddr *) server, sizeof(*server)) < 0) {
         printf("Error: failed to bind to port %d\n", port_no);
         exit(1);
     }
 
     *server_sock_ptr = server_sock;
-    *server_ptr = server;
+}
+
+
+int validate_message(char **buf, char **msg) {
+    return 1;
+}
+
+char *str_join(char *buf, char *add) {
+    char *newbuf;
+    int  len;
+
+    if (buf == 0)
+        len = 0;
+    else
+        len = strlen(buf);
+
+    newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+    if (newbuf == 0)
+        return (0);
+
+    newbuf[0] = 0;
+    if (buf != 0)
+        strcat(newbuf, buf);
+
+    free(buf);
+    strcat(newbuf, add);
+    return (newbuf);
+}
+
+void free_client(client_t *cli) {
+    if (cli) {
+        if (cli->msg) 
+            free(cli->msg);
+        if (cli->fd > 0) 
+            close(cli->fd);
+        free(cli);
+    }
 }
 
 client_t *add_client(server_t *s, int fd) {
@@ -53,7 +88,7 @@ client_t *add_client(server_t *s, int fd) {
     return cli;
 }
 
-client_t *findClient(server_t *s, int fd) {
+client_t *find_client(server_t *s, int fd) {
     client_t *tmp = s->head;
 
     while (tmp && tmp->fd != fd)
@@ -74,7 +109,7 @@ void remove_client(server_t *s, int fd) {
             prev->next = tmp->next;
         else
             s->head = tmp->next;
-        freeClient(tmp);
+        free_client(tmp);
     }
 }
 
@@ -91,10 +126,10 @@ void send_notification(server_t *s, int fd, char *msg) {
     }
 }
 
-void sendMessage(server_t *s, client_t *cli) {
+void send_message(server_t *s, client_t *cli) {
     char buf[127];
     char *msg;
-    while (extract_message(&cli->msg, &msg))
+    while (validate_message(&cli->msg, &msg))
     {
         if (FD_ISSET(cli->fd, &s->writefds)) {
             sprintf(buf, "client %d: ", cli->id);
@@ -105,7 +140,7 @@ void sendMessage(server_t *s, client_t *cli) {
     }
 }
 
-void deregisterClient(server_t *s, int fd, int cli_id) {
+void deregister_client(server_t *s, int fd, int cli_id) {
     char buf[127];
     sprintf(buf, "server: client %d just left\n", cli_id);
     send_notification(s, fd, buf);
@@ -113,24 +148,26 @@ void deregisterClient(server_t *s, int fd, int cli_id) {
     remove_client(s, fd);
 }
 
-void processMessage(server_t *s, int fd) {
+void process_message(server_t *s, int fd) {
     char buf[4096];
-    client_t *cli = findClient(s, fd);
+    client_t *cli = find_client(s, fd);
     if (!cli) return;
     int read_bytes = recv(fd, buf, sizeof(buf) - 1, 0);
     if (read_bytes <= 0) {
-        deregisterClient(s, fd, cli->id);
+        deregister_client(s, fd, cli->id);
     } else {
         buf[read_bytes] = '\0';
         cli->msg = str_join(cli->msg, buf);
-        sendMessage(s, cli);
+        send_message(s, cli);
     }
 }
 
-void registerClient(server_t *s, int fd) {
+void register_client(server_t *s, int fd) {
     client_t *cli = add_client(s, fd);
     char buf[127];
-    if (!cli) fatalError(s);
+    if (!cli) {
+        printf("Client wasn't added\n");
+    }
     FD_SET(cli->fd, &s->active_fds);
     if (cli->fd > s->max_fd)
         s->max_fd = cli->fd;
@@ -138,21 +175,25 @@ void registerClient(server_t *s, int fd) {
     send_notification(s, fd, buf);
 }
 
-void acceptRegistration(server_t *s) {
+void accept_registration(server_t *s) {
     struct sockaddr_in cli; 
 
     socklen_t len = sizeof(cli);
     int fd = accept(s->sockfd, (struct sockaddr *)&cli, &len);
-    if (fd < 0) fatalError(s);
-    registerClient(s, fd);
+    if (fd < 0) {
+        printf("Unable to accept the new connection");
+    }
+    register_client(s, fd);
 }
 
 void monitor_fds(server_t *s) {
-    if (select(s->max_fd + 1, &s->readfds, &s->writefds, NULL, NULL) < 0) fatalError(s);
+    if (select(s->max_fd + 1, &s->readfds, &s->writefds, NULL, NULL) < 0) {
+        printf("Error");
+    }
     int fd = 0;
     while (fd <= s->max_fd) {
         if (FD_ISSET(fd, &s->readfds))
-            (fd == s->sockfd) ? acceptRegistration(s) : processMessage(s, fd);
+            (fd == s->sockfd) ? accept_registration(s) : process_message(s, fd);
         fd++;
     }
 }
@@ -165,14 +206,39 @@ void handle_connection(server_t *s) {
     }
 }
 
+server_t *init_server(int port) {
+    server_t *s = (server_t *) malloc(sizeof(server_t));
+    if (!s) {
+        printf("Failed to allocate memory to the server.\n");
+    }
+
+    memset(s, 0, sizeof(server_t));
+    FD_ZERO(&s->active_fds);
+    FD_ZERO(&s->readfds);
+    FD_ZERO(&s->writefds);
+    s->port = port;
+    return s;
+}
+
 int main() {
-    struct sockaddr_in server_in, server_out, remaddr;
-    socklen_t addrlen = sizeof(remaddr);
-    int server_in_sock, server_out_sock, conn_sock;
+    struct sockaddr_in server_in, server_out;
+    int server_in_sock, server_out_sock;
 
     make_socket(&server_in, &server_in_sock, SOURCE_PORT);
     make_socket(&server_out, &server_out_sock, DESTINATION_PORT);
 
-    printf("Waiting on Port %d\n", SOURCE_PORT);
-    
+    pid_t p;
+    p = fork();
+    if(p<0) {
+      printf("Fork fail\n");
+      exit(1);
+    } else if (p == 0) {
+        server_t * receiving_server = init_server(DESTINATION_PORT);
+        printf("Waiting on Port %d\n", DESTINATION_PORT);
+        handle_connection(receiving_server);
+    } else {
+        server_t * sending_server = init_server(SOURCE_PORT);
+        printf("Waiting on Port %d\n", SOURCE_PORT);
+        handle_connection(sending_server);
+    }    
 }
